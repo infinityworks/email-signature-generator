be79c5c224efb9223a960fc35cf7c60da23297d6
Pete Craven <petecraven@Petes-MacBook-Pro.local>
2018-11-30 09:45:39 +0000

    Created signature generator

---
+++ index.js
@@ -0,0 +1,14 @@
+const handlebars = require('handlebars');
+const fs = require('fs');
+
+const config = require('./config.json');
+
+handlebars.registerHelper('toUpperCase', function(str) {
+    return str.toUpperCase();
+});
+
+var template = handlebars.compile(fs.readFileSync('./signature.html', 'utf8'));
+var generated = template(config);
+
+fs.writeFileSync('./generated.html', generated, 'utf-8');
+
